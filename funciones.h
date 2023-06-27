c82d0da8bda7686b920bb988e0b77767a41e1e01
singooscar <137453325+singooscar@users.noreply.github.com>
2023-06-27 13:58:50 -0500

    funciones.h

--- funciones.h
+++ funciones.h
@@ -1,19 +1,9 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
-#define MAX_PRODUCTOS 10
-
-typedef struct {
-    int id;
-    char nombre[20];
-    char descripcion[50];
-    float precio;
-    int cantidad;
-} Producto;
-
-void imprimirListaProductos(Producto* productos, int numProductos);
-void ingresarProductos(Producto* productos, int* numProductos);
-void modificarProducto(Producto* productos, int numProductos);
-void eliminarProducto(Producto* productos, int* numProductos);
+void imprimirListaProductos(int* id, char nombre[][20], char descripcion[][50], float* precio, int* cantidad, int numProductos);
+void ingresarProductos(int* id, char nombre[][20], char descripcion[][50], float* precio, int* cantidad, int* numProductos);
+void modificarProducto(int* id, char nombre[][20], char descripcion[][50], float* precio, int* cantidad, int numProductos);
+void eliminarProducto(int* id, char nombre[][20], char descripcion[][50], float* precio, int* cantidad, int* numProductos);
 
 #endif  // FUNCIONES_H
